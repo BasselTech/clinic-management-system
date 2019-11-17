adf222e52f0cd77cbd4cf8ac1429a7b28e63414b
Bassel A. Elerian <basselakmal@gmail.com>
2019-11-17 20:41:27 +0200

    CMS - Part 13 - Secretary Panel - Create Reservation

--- SecretaryPanel.cs
+++ SecretaryPanel.cs
@@ -34,5 +34,13 @@ namespace Clinic_Management_System
             patientProfiles.ShowDialog();
             Show();
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Hide();
+            CreateReservation createReservation = new CreateReservation(account_id);
+            createReservation.ShowDialog();
+            Show();
+        }
     }
 }
